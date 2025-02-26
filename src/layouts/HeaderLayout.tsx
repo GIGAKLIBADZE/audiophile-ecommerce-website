d669d4700fb5dddcbb5125e6c9d6c3c85396f2fa
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-02-27 00:00:01 +0400

    add the structure of the header layout

--- src/layouts/HeaderLayout.tsx
+++ src/layouts/HeaderLayout.tsx
@@ -1,5 +1,18 @@
+import Burger from "/images/shared/tablet/icon-hamburger.svg";
+import Title from "/images/shared/desktop/logo.svg";
+import Basket from "/images/shared/desktop/icon-cart.svg";
+
 const HeaderLayout: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <header>
+        <img src={Burger} alt="Menu" />
+        <img src={Title} alt="Title" />
+        <img src={Basket} alt="Basket" />
+        <div></div>
+      </header>
+    </div>
+  );
 };
 
 export default HeaderLayout;
