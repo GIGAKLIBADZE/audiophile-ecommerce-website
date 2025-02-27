937c6d6a2a2b1fd4b083411512db86cffae1d8cd
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-02-27 17:28:05 +0400

    add styles for the footer layout

--- src/components/FooterLayoutStyles.tsx
+++ src/components/FooterLayoutStyles.tsx
@@ -1,5 +1,45 @@
 import styled from "styled-components";
 
 export const FooterContainer = styled.footer`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 5.2rem 2.4rem 3.8rem;
   background-color: #101010;
+`;
+
+export const FooterMenu = styled.nav`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.6rem;
+  font-size: 1.3rem;
+  font-weight: bold;
+  line-height: 1.92;
+  letter-spacing: 2px;
+  color: #fff;
+  margin-top: 4.8rem;
+`;
+
+export const FooterInfoAndSocialMediaContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 4.8rem;
+`;
+
+export const FooterInfo = styled.p`
+  font-size: 1.5rem;
+  font-weight: normal;
+  line-height: 1.67;
+  text-align: center;
+  color: #fff;
+  opacity: 0.5;
+`;
+
+export const SocialMediaContainer = styled.section`
+  display: flex;
+  gap: 1.6rem;
+  margin-top: 5rem;
 `;
--- src/layouts/FooterLayout.tsx
+++ src/layouts/FooterLayout.tsx
@@ -2,7 +2,14 @@ import Title from "/images/shared/desktop/logo.svg";
 import Facebook from "/images/shared/desktop/icon-facebook.svg";
 import Instagram from "/images/shared/desktop/icon-instagram.svg";
 import Twitter from "/images/shared/desktop/icon-twitter.svg";
-import { FooterContainer } from "../components/FooterLayoutStyles";
+import {
+  FooterContainer,
+  FooterInfo,
+  FooterInfoAndSocialMediaContainer,
+  // FooterInfoContainer,
+  FooterMenu,
+  SocialMediaContainer,
+} from "../components/FooterLayoutStyles";
 
 const FooterLayout: React.FC = () => {
   return (
@@ -10,27 +17,31 @@ const FooterLayout: React.FC = () => {
       <FooterContainer>
         <div>
           <img src={Title} alt="Title" />
-          <nav>
+          <FooterMenu>
             <h6>HOME</h6>
             <h6>HEADPHONES</h6>
             <h6>SPEAKERS</h6>
             <h6>EARPHONES</h6>
-          </nav>
+          </FooterMenu>
         </div>
-        <div>
-          <p>
-            Audiophile is an all in one stop to fulfill your audio needs. We're
-            a small team of music lovers and sound specialists who are devoted
-            to helping you get the most out of personal audio. Come and visit
-            our demo facility - we’re open 7 days a week.
-          </p>
-          <span>Copyright 2021. All Rights Reserved</span>
-          <section>
+        <FooterInfoAndSocialMediaContainer>
+          <div>
+            <FooterInfo>
+              Audiophile is an all in one stop to fulfill your audio needs.
+              We're a small team of music lovers and sound specialists who are
+              devoted to helping you get the most out of personal audio. Come
+              and visit our demo facility - we’re open 7 days a week.
+            </FooterInfo>
+            <FooterInfo style={{ fontWeight: "bold", marginTop: "4.8rem" }}>
+              Copyright 2021. All Rights Reserved
+            </FooterInfo>
+          </div>
+          <SocialMediaContainer>
             <img src={Facebook} alt="Facebook icon" />
             <img src={Instagram} alt="Instagram icon" />
             <img src={Twitter} alt="Twitter icon" />
-          </section>
-        </div>
+          </SocialMediaContainer>
+        </FooterInfoAndSocialMediaContainer>
       </FooterContainer>
     </div>
   );
