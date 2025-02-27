4bbbce0fadbc788678387ed660c6bf01afd6ab55
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-02-27 16:46:34 +0400

    add styles for the header layout

--- src/components/HeaderLayoutStyles.tsx
+++ src/components/HeaderLayoutStyles.tsx
@@ -1,6 +1,17 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
+  width: 100%;
   padding: 3.2rem 2.4rem;
   background-color: #191919;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Line = styled.div`
+  width: 100%;
+  height: 0.1rem;
+  opacity: 0.1;
+  background-color: #fff;
 `;
--- src/layouts/HeaderLayout.tsx
+++ src/layouts/HeaderLayout.tsx
@@ -1,16 +1,17 @@
 import Burger from "/images/shared/tablet/icon-hamburger.svg";
 import Title from "/images/shared/desktop/logo.svg";
 import Basket from "/images/shared/desktop/icon-cart.svg";
+import { HeaderContainer, Line } from "../components/HeaderLayoutStyles";
 
 const HeaderLayout: React.FC = () => {
   return (
     <div>
-      <header>
+      <HeaderContainer>
         <img src={Burger} alt="Menu" />
         <img src={Title} alt="Title" />
         <img src={Basket} alt="Basket" />
-        <div></div>
-      </header>
+      </HeaderContainer>
+      <Line></Line>
     </div>
   );
 };
