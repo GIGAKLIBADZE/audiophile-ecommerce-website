b89368c9d9df516682fde3e340950919f646be9e
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-03-02 21:50:53 +0400

    initialize the footer part of the home page

---
+++ src/components/HomeFooter.tsx
@@ -0,0 +1,5 @@
+const HomeFooter: React.FC = () => {
+  return <footer></footer>;
+};
+
+export default HomeFooter;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import { fetchInformation } from "../features/data/dataSlice";
 import { AppDispatch, RootState } from "../store";
 import { useEffect } from "react";
 import HomeProducts from "../components/HomeProducts";
+import HomeFooter from "../components/HomeFooter";
 
 const Home: React.FC = () => {
   const data = useSelector((store: RootState) => store.fetchedInformation.info);
@@ -19,6 +20,7 @@ const Home: React.FC = () => {
       <HomeHeader />
       <HomeCategories data={data} />
       <HomeProducts data={data} />
+      <HomeFooter />
     </div>
   );
 };
