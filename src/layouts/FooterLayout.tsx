aa115a3fc23066a126038077f35af1558363f8f8
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-03-03 19:46:46 +0400

    move that last part of the home into the footer layout

--- src/layouts/FooterLayout.tsx
+++ src/layouts/FooterLayout.tsx
@@ -6,14 +6,32 @@ import {
   FooterContainer,
   FooterInfo,
   FooterInfoAndSocialMediaContainer,
-  // FooterInfoContainer,
   FooterMenu,
   SocialMediaContainer,
 } from "../components/home/FooterLayoutStyles";
+import BestGear from "/images/shared/mobile/image-best-gear.jpg";
 
 const FooterLayout: React.FC = () => {
   return (
     <div>
+      <div>
+        <img
+          src={BestGear}
+          alt="Best gear"
+          style={{ width: "4rem", height: "4rem" }}
+        />
+      </div>
+      <div>
+        <h4>Bringing you the best audio gear</h4>
+        <p>
+          Located at the heart of New York City, Audiophile is the premier store
+          for high end headphones, earphones, speakers, and audio accessories.
+          We have a large showroom and luxury demonstration rooms available for
+          you to browse and experience a wide range of our products. Stop by our
+          store to meet some of the fantastic people who make Audiophile the
+          best place to buy your portable audio equipment.
+        </p>
+      </div>
       <FooterContainer>
         <div>
           <img src={Title} alt="Title" />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,9 @@
 import HomeHeader from "../components/home/HomeHeader";
-// import HomeCategories from "../components/home/HomeCategories";
-// import HomeCategories from "../components/HomeCategories";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchInformation } from "../features/data/dataSlice";
 import { AppDispatch, RootState } from "../store";
 import { useEffect } from "react";
 import HomeProducts from "../components/home/HomeProducts";
-import HomeFooter from "../components/home/HomeFooter";
 import HomeCategories from "../components/home/HomeCategories";
 
 const Home: React.FC = () => {
@@ -22,7 +19,6 @@ const Home: React.FC = () => {
       <HomeHeader />
       <HomeCategories data={data} />
       <HomeProducts data={data} />/
-      <HomeFooter />
     </div>
   );
 };
