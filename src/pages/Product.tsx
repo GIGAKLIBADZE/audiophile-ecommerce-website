1ffa4f6ca600e8e01dba68f4371b9b1c49ae39b7
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-03-06 18:58:42 +0400

    initialize the component for a particular product pictures and its styles

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,11 +1,13 @@
 import ProductDetails from "../components/product/ProductDetails";
 import ProductDescription from "../components/product/ProductDescription";
+import ProductPictures from "../components/product/ProductPictures";
 
 const Product: React.FC = () => {
   return (
     <>
       <ProductDetails />
       <ProductDescription />
+      <ProductPictures />
     </>
   );
 };
