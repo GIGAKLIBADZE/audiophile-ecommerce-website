45dbaa0dec744d94f0422a06d42058bd99b661a7
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-03-07 14:14:23 +0400

    Style ProductDescription.tsx file

--- src/components/product/ProductDescription.tsx
+++ src/components/product/ProductDescription.tsx
@@ -3,6 +3,16 @@ import { RootState } from "../../store";
 import { IItem } from "../../types/types";
 import { useParams } from "react-router-dom";
 import { TIncludeItem } from "../../types/types";
+import {
+  BoxContainer,
+  DescriptionContainer,
+  DetailsHeadLine,
+  Features,
+  List,
+  ListItem,
+  ListItemAmount,
+  Item,
+} from "./ProductDescriptionStyles";
 
 const ProductDescription: React.FC = () => {
   const { slug } = useParams();
@@ -18,37 +28,43 @@ const ProductDescription: React.FC = () => {
   }
 
   return (
-    <div>
+    <DescriptionContainer>
       <div>
-        <h3>FEATURES</h3>
-        <p>
+        <DetailsHeadLine>FEATURES</DetailsHeadLine>
+        <Features>
           Featuring a genuine leather head strap and premium earcups, these
           headphones deliver superior comfort for those who like to enjoy
           endless listening. It includes intuitive controls designed for any
           situation. Whether you’re taking a business call or just in your own
           personal space, the auto on/off and pause features ensure that you’ll
-          never miss a beat. The advanced Active Noise Cancellation with
-          built-in equalizer allow you to experience your audio world on your
-          terms. It lets you enjoy your audio in peace, but quickly interact
-          with your surroundings when you need to. Combined with Bluetooth 5. 0
-          compliant connectivity and 17 hour battery life, the XX99 Mark II
-          headphones gives you superior sound, cutting-edge technology, and a
-          modern design aesthetic.
-        </p>
+          never miss a beat.
+        </Features>
+        <Features style={{ marginTop: "2.4rem" }}>
+          The advanced Active Noise Cancellation with built-in equalizer allow
+          you to experience your audio world on your terms. It lets you enjoy
+          your audio in peace, but quickly interact with your surroundings when
+          you need to. Combined with Bluetooth 5. 0 compliant connectivity and
+          17 hour battery life, the XX99 Mark II headphones gives you superior
+          sound, cutting-edge technology, and a modern design aesthetic.
+        </Features>
       </div>
-      <div>
-        <h3>IN THE BOX</h3>
-        <ul>
+      <BoxContainer>
+        <DetailsHeadLine>IN THE BOX</DetailsHeadLine>
+        <List>
           {product.includes.map((i: TIncludeItem) => (
-            <li key={i.item}>
-              <p>
-                <span>{i.quantity}x</span> {i.item}
-              </p>
-            </li>
+            <ListItem key={i.item}>
+              <ListItemAmount>{i.quantity}x</ListItemAmount>
+              <Item>
+                {i.item
+                  .split(" ")
+                  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                  .join(" ")}
+              </Item>
+            </ListItem>
           ))}
-        </ul>
-      </div>
-    </div>
+        </List>
+      </BoxContainer>
+    </DescriptionContainer>
   );
 };
 
--- src/components/product/ProductDescriptionStyles.tsx
+++ src/components/product/ProductDescriptionStyles.tsx
@@ -1,1 +1,52 @@
 import styled from "styled-components";
+
+export const DescriptionContainer = styled.div`
+  width: 87.2%;
+  margin-inline: auto;
+  margin-top: 8.8rem;
+`;
+
+export const DetailsHeadLine = styled.h3`
+  font-size: 2.4rem;
+  font-weight: bold;
+  line-height: 1.5;
+  letter-spacing: 0.86px;
+  color: #000;
+`;
+
+export const Features = styled.span`
+  display: block;
+  font-size: 1.5rem;
+  font-weight: 500;
+  line-height: 1.67;
+  color: rgba(0, 0, 0, 0.5);
+  margin-top: 2.4rem;
+`;
+
+export const BoxContainer = styled.div`
+  margin-top: 8.8rem;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  list-style: none;
+  margin-top: 2.4rem;
+`;
+
+export const ListItem = styled(Features)`
+  display: flex;
+  align-items: center;
+  gap: 2.4rem;
+`;
+
+export const ListItemAmount = styled(Features)`
+  font-weight: bold;
+  color: #d87d4a;
+  margin-top: 0;
+`;
+
+export const Item = styled(Features)`
+  margin-top: 0;
+`;
