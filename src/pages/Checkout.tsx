0c10d406fda02f2d4f763177db26be6bc4efa9a6
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-04-13 13:21:18 +0400

    add the billing details part

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,5 +1,34 @@
+import { GoBack } from "../components/product/ProductDetailsStyles";
+
 const Checkout: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <GoBack>Go Back</GoBack>
+      <div>
+        <div>
+          <h5>CHECKOUT</h5>
+          <p>BILLING DETAILS</p>
+          <form>
+            <div>
+              <div>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" placeholder="Alexei Ward" />
+              </div>
+              <div>
+                <label htmlFor="email">Email Address</label>
+                <input type="text" id="email" placeholder="alexei@mail.com" />
+              </div>
+              <div>
+                <label htmlFor="phone">Phone Number</label>
+                <input type="text" id="phone" placeholder="+1 202-555-0136" />
+              </div>
+            </div>
+          </form>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
