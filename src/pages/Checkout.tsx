4b865e121a85dc395cd2a21837c0c6b628349be8
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-04-14 15:59:31 +0400

    Add the structure of shipping info

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -7,20 +7,47 @@ const Checkout: React.FC = () => {
       <div>
         <div>
           <h5>CHECKOUT</h5>
-          <p>BILLING DETAILS</p>
           <form>
             <div>
+              <p>BILLING DETAILS</p>
               <div>
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" placeholder="Alexei Ward" />
+                <div>
+                  <label htmlFor="name">Name</label>
+                  <input type="text" id="name" placeholder="Alexei Ward" />
+                </div>
+                <div>
+                  <label htmlFor="email">Email Address</label>
+                  <input type="text" id="email" placeholder="alexei@mail.com" />
+                </div>
+                <div>
+                  <label htmlFor="phone">Phone Number</label>
+                  <input type="text" id="phone" placeholder="+1 202-555-0136" />
+                </div>
               </div>
+            </div>
+            <div>
+              <p>SHIPPING INFO</p>
               <div>
-                <label htmlFor="email">Email Address</label>
-                <input type="text" id="email" placeholder="alexei@mail.com" />
-              </div>
-              <div>
-                <label htmlFor="phone">Phone Number</label>
-                <input type="text" id="phone" placeholder="+1 202-555-0136" />
+                <div>
+                  <label htmlFor="address">Your Address</label>
+                  <input
+                    type="text"
+                    id="address"
+                    placeholder="1137 Williams Avenue"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="zip-code">Zip Code</label>
+                  <input type="number" id="zip-code" placeholder="10001" />
+                </div>
+                <div>
+                  <label htmlFor="city">City</label>
+                  <input type="text" id="city" placeholder="New York" />
+                </div>
+                <div>
+                  <label htmlFor="country">Country</label>
+                  <input type="text" id="country" placeholder="United States" />
+                </div>
               </div>
             </div>
           </form>
